0cce45652d592ba78b4decd41d76d0e0f7a51a2d
Gabriel Castro <gabriel.castro@leriand.com>
2024-02-14 19:32:16 -0300

    feat: add DataTable component

--- src/app/ledgers/page.tsx
+++ src/app/ledgers/page.tsx
@@ -1,9 +1,35 @@
+import { DataTable } from '@/components/DataTable'
 import { PageTitle } from '@/components/PageTitle'
+import { ColumnDef } from '@tanstack/react-table'
+
+type Payment = {
+  id: string
+  name: string
+}
+
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'name',
+    header: 'Name'
+  }
+]
+
+const data: Payment[] = [
+  {
+    id: '728ed52f',
+    name: 'wallet1'
+  },
+  {
+    id: '489e1d42',
+    name: 'wallet2'
+  }
+]
 
 const Page = () => {
   return (
-    <div>
+    <div className="flex w-full flex-col gap-5">
       <PageTitle title="Ledgers" />
+      <DataTable columns={columns} data={data} />
     </div>
   )
 }
