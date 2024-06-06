f0296a6e441fd7dff07418e27dabba71cf6e3cca
Augusto de Alvarenga Mesquita <augusto.santesso@gmail.com>
2024-06-06 19:58:34 -0300

    add cors headers

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,19 @@ const nextConfig = {
     MIDAZ_CONSOLE_BASE_PATH: process.env.MIDAZ_CONSOLE_BASE_PATH,
     MIDAZ_SERVER_BASE_PATH: process.env.MIDAZ_SERVER_BASE_PATH,
     ORY_KRATOS_PUBLIC_URL: process.env.ORY_KRATOS_PUBLIC_URL
+  },
+  headers: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
   }
 }
 
