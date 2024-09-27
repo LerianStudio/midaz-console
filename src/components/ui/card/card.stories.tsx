12046611a520081765e744588d3f884cd009efef
Caio Alexandre Troti Caetano <caio.troti@lerian.studio>
2024-09-27 14:09:52 -0300

    Adjusted card component

---
+++ src/components/ui/card/card.stories.tsx
@@ -0,0 +1,47 @@
+import { Meta, StoryObj } from '@storybook/react'
+import {
+  Card,
+  CardTitle,
+  CardProps,
+  CardHeader,
+  CardDescription,
+  CardContent,
+  CardFooter
+} from './card'
+import { Button } from '../button/button'
+
+const meta: Meta<CardProps> = {
+  title: 'Molecules/Card',
+  component: Card,
+  subcomponents: {
+    CardTitle,
+    CardHeader,
+    CardDescription,
+    CardContent,
+    CardFooter
+  },
+  argTypes: {
+    className: {
+      type: 'string',
+      description: 'The card class'
+    }
+  }
+}
+
+export default meta
+
+export const Primary: StoryObj<CardProps> = {
+  render: (args) => (
+    <Card className="w-[350px]" {...args}>
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>This is the Card Content</CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+  )
+}
