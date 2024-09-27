2029e6ab77c0ccad1fada9fc9948d9c419945848
Caio Alexandre Troti Caetano <caio.troti@lerian.studio>
2024-09-27 16:01:37 -0300

    Added tests

--- src/hooks/use-tabs.tsx
+++ src/hooks/use-tabs.tsx
@@ -1,17 +1,28 @@
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
+import { isNil } from 'lodash'
 
 export type UseTabsProps = {
   initialValue?: string
   onTabChange?: (tab: string) => void
 }
 
+/**
+ * Hook designed to simplify usage of Tabs together with other components
+ * that needs this information. Ex: Breadcrumb
+ * @param param0
+ * @returns
+ */
 export const useTabs = ({ initialValue, onTabChange }: UseTabsProps) => {
   const router = useRouter()
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const [activeTab, setActiveTab] = React.useState(initialValue || '')
 
+  /**
+   * Update state and route application with the respective tab as a URL search param
+   * @param tab
+   */
   const handleTabChange = (tab: string) => {
     setActiveTab(tab)
     router.push(`${pathname}?${createQueryString('tab', tab)}`)
@@ -32,8 +43,19 @@ export const useTabs = ({ initialValue, onTabChange }: UseTabsProps) => {
     [searchParams]
   )
 
+  /**
+   * Updates activeTab when changed from URL parameters
+   */
   React.useEffect(() => {
-    handleTabChange(activeTab)
+    const params = new URLSearchParams(searchParams.toString())
+    const tab = params.get('tab')
+
+    // Avoid if no tab params is found
+    if (isNil(tab)) {
+      return
+    }
+
+    setActiveTab(tab)
   }, [searchParams])
 
   return {
