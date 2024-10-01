3f2c94d74dca6b074d221296dae25ea62686ce48
Caio Alexandre Troti Caetano <caio.troti@lerian.studio>
2024-10-01 16:27:32 -0300

    Solved image problem

--- src/components/organization-switcher/index.tsx
+++ src/components/organization-switcher/index.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React from 'react'
 import { Popover } from '@/components/ui/popover'
 import { SwitcherTrigger } from './organization-switcher-trigger'
--- src/components/organization-switcher/organization-switcher-trigger.tsx
+++ src/components/organization-switcher/organization-switcher-trigger.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from './ClientImage'
 import { cn } from '@/lib/utils'
 import { PopoverTrigger } from '../ui/popover'
 import { ChevronDown } from 'lucide-react'
