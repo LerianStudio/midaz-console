0b734f9ef36da28f6219b21f080c0b5ccef1687c
paulolerian <paulo.barbosa@lerian.studio>
2024-10-16 16:20:32 -0300

    page adjst

--- src/app/(routes)/ledgers/[id]/page.tsx
+++ src/app/(routes)/ledgers/[id]/page.tsx
@@ -4,7 +4,6 @@ import { FormDetailsProvider } from '@/context/form-details-context'
 import LedgerDetailsView from './ledger-details-view'
 import { useLedgerById } from '@/utils/queries'
 import { FormProvider, useForm } from 'react-hook-form'
-import jsonData from '@/../faker/index.json'
 type Params = {
   params: {
     locale: string
@@ -13,9 +12,8 @@ type Params = {
 }
 
 const Page = ({ params }: Params) => {
-  // const data = useLedgerById(params.id)
+  const data = useLedgerById(params.id)
   const methods = useForm()
-  const data = { data: jsonData.ledgers }
 
   return (
     <FormDetailsProvider>
