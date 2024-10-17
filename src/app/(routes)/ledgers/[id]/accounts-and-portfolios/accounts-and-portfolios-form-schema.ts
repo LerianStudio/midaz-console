9557fe743121ff55eb24056da3e93f106cd518b7
paulolerian <paulo.barbosa@lerian.studio>
2024-10-17 15:42:29 -0300

    remove entity

--- src/app/(routes)/ledgers/[id]/accounts-and-portfolios/accounts-and-portfolios-form-schema.ts
+++ src/app/(routes)/ledgers/[id]/accounts-and-portfolios/accounts-and-portfolios-form-schema.ts
@@ -3,6 +3,6 @@ import { z } from 'zod'
 
 export const formSchemaPortfolio = z.object({
   name: z.string().min(3),
-  entityId: z.string().min(1, 'Entity ID is required').optional(),
+  entityId: z.string().min(1).optional(),
   metadata: metadata
 })
