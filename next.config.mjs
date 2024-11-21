022600063acbe996a7fb642a969e07109abb5f98
LF Barrile <lf@lerian.studio>
2024-11-21 12:18:19 -0300

    set the value of hostname

--- next.config.mjs
+++ next.config.mjs
@@ -35,7 +35,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: process.env.MIDAZ_CONSOLE_SERVICE_HOST,
+        hostname: process.env.MIDAZ_CONSOLE_SERVICE_HOST || 'localhost',
         pathname: '**'
       }
     ]
