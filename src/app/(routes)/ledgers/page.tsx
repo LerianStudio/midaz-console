ff654afc066a536a0403aa29edda24fb94612abf
Gabriel Castro <gabrielcastro.xy@gmail.com>
2024-11-26 10:29:30 -0300

    :boom: fix: ledgers page refetch

--- src/app/(routes)/ledgers/page.tsx
+++ src/app/(routes)/ledgers/page.tsx
@@ -2,17 +2,35 @@
 
 import LedgersView from '@/app/(routes)/ledgers/ledgers-view'
 import { useListLedgers } from '@/client/ledgers'
+import { useListOrganizations } from '@/client/organizations'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { Button } from '@/components/ui/button'
 import { useOrganization } from '@/context/organization-provider/organization-provider-client'
 import { AlertCircle } from 'lucide-react'
 import { useRouter } from 'next/navigation'
+import React from 'react'
 import { useIntl } from 'react-intl'
 
 const Page = () => {
-  const { currentOrganization } = useOrganization()
+  const { currentOrganization, setOrganization } = useOrganization()
+  const { data: organizations } = useListOrganizations({})
   const intl = useIntl()
   const router = useRouter()
+
+  React.useEffect(() => {
+    if (!currentOrganization && organizations?.items?.length! > 0) {
+      setOrganization(organizations!.items[0])
+    }
+  }, [currentOrganization, organizations, setOrganization])
+
+  const {
+    data: ledgers,
+    refetch,
+    isLoading
+  } = useListLedgers({
+    organizationId: currentOrganization?.id!,
+    enabled: !!currentOrganization
+  })
 
   if (!currentOrganization) {
     return (
@@ -48,14 +66,6 @@ const Page = () => {
       </div>
     )
   }
-
-  const {
-    data: ledgers,
-    refetch,
-    isLoading
-  } = useListLedgers({
-    organizationId: currentOrganization.id!
-  })
 
   return (
     <LedgersView ledgers={ledgers} refetch={refetch} isLoading={isLoading} />
