6f95048bda612a29de834310ec13118d0624fc30
LF Barrile <lf@lerian.studio>
2024-12-09 19:29:55 -0300

    chore: release

--- release.config.js
+++ release.config.js
@@ -7,12 +7,12 @@ module.exports = {
     "next-major",
     { name: "beta", prerelease: "beta" },
     { name: "alpha", prerelease: "alpha" },
-    { name: "feature/authorization", prerelease: "feature-authorization" },
-    { name: "feature/implement-githooks", prerelease: "feature-implement-githooks" },
-    { name: "feature/input-with-tooltip", prerelease: "feature-input-with-tooltip" },
-    { name: "feature/logger", prerelease: "feature-logger" },
-    { name: "feature/organize-diagrams", prerelease: "feature-organize-diagrams" },
-    { name: "feature/set-email-notification", prerelease: "feature-set-email-notification" }
+    { name: "feature/authorization", prerelease: "feature-authorization" }, //remember to remove this lines
+    { name: "feature/implement-githooks", prerelease: "feature-implement-githooks" }, //remember to remove this lines
+    { name: "feature/input-with-tooltip", prerelease: "feature-input-with-tooltip" }, //remember to remove this lines
+    { name: "feature/logger", prerelease: "feature-logger" }, //remember to remove this lines
+    { name: "feature/organize-diagrams", prerelease: "feature-organize-diagrams" },//remember to remove this lines
+    { name: "feature/set-email-notification", prerelease: "feature-set-email-notification" } //remember to remove this lines
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
