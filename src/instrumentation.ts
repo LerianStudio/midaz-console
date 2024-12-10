3a23f80dce8a1444749eaee451a37817fa42ea25
Gabriel Castro <gabrielcastro.xy@gmail.com>
2024-12-10 13:20:52 -0300

    :recycle: refactor: move instrumentation-config file to core/infrastructure/observability

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,5 @@
-export const register = async () => {
+export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./instrumentation-node')
+    await import('./core/infrastructure/observability/instrumentation-config')
   }
 }
