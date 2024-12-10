58eb5321ba0d02ca619fb30dd5f7ed918ef6a857
paulolerian <paulo.barbosa@lerian.studio>
2024-12-10 18:19:54 -0300

    remove unused code

--- src/core/domain/logger/logger.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface ILogger {
-  debug(message: string, metadata?: Record<string, any>): void
-  info(message: string, metadata?: Record<string, any>): void
-  warn(message: string, metadata?: Record<string, any>): void
-  error(message: string, error?: Error, metadata?: Record<string, any>): void
-  audit(message: string, metadata?: Record<string, any>): void
-}
