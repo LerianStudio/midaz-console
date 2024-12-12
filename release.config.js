1f229b2754567abe717ea0c44e176291fdc91651
LF Barrile <lf@lerian.studio>
2024-12-12 17:00:23 -0300

    set the structure to generate release

--- release.config.js
+++ release.config.js
@@ -1,18 +1,7 @@
 module.exports = {
   branches: [
     "main",
-    "develop",
-    "hotfix/*",
-    "next",
-    "next-major",
-    { name: "beta", prerelease: "beta" },
-    { name: "alpha", prerelease: "alpha" },
-    { name: "feature/authorization", prerelease: "feature-authorization" }, //remember to remove this lines
-    { name: "feature/implement-githooks", prerelease: "feature-implement-githooks" }, //remember to remove this lines
-    { name: "feature/input-with-tooltip", prerelease: "feature-input-with-tooltip" }, //remember to remove this lines
-    { name: "feature/logger", prerelease: "feature-logger" }, //remember to remove this lines
-    { name: "feature/organize-diagrams", prerelease: "feature-organize-diagrams" },//remember to remove this lines
-    { name: "feature/set-email-notification", prerelease: "feature-set-email-notification" } //remember to remove this lines
+    "develop"
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
