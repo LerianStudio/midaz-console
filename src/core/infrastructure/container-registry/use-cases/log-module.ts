5bb9960cc3b2e12f82ffd903dc9f6a3f96ea6cec
paulobarbosa <contatopaulofreire96@gmail.com>
2025-01-09 15:11:13 -0300

    feat: remove log-container

--- src/core/infrastructure/container-registry/use-cases/log-module.ts
+++
@@ -1,9 +0,0 @@
-import { Container } from 'inversify'
-import { LoggerRepository } from '@/core/domain/repositories/logger/logger-repository'
-import { PinoLoggerRepository } from '@/core/infrastructure/logger/pino-logger-repository'
-
-const container = new Container()
-
-container.bind<LoggerRepository>('LoggerRepository').to(PinoLoggerRepository)
-
-export { container }
