a2bd7487fa722cd3672bcfb73407e6ce4c9b6006
LF Barrile <lf@lerian.studio>
2025-01-20 18:05:11 -0300

    fix: separate the flow of releases on midaz-console

--- release.config.js
+++ release.config.js
@@ -1,18 +1,42 @@
 module.exports = {
   branches: [
-    { name: 'main', channel: 'latest' },    // Prod
-    { name: 'develop', channel: 'next' },    // Dev
+    { name: 'main', channel: 'latest' }, // Prod
+    { name: 'develop', channel: 'next' }, // Dev
+    {
+      name: 'patch/*', // Branches begin with "patch/"
+      channel: 'patch',
+      prerelease: 'patch', // Generate pre-releases for "patch/*"
+    },
+    {
+      name: 'fix/*', // Branches begin with "fix/"
+      channel: 'fix',
+      prerelease: 'fix', // Generate pre-releases for "fix/*"
+    },
+    {
+      name: 'feature/*', // Branches begin with "feature/"
+      channel: 'feature',
+      prerelease: 'feature', // Generate pre-releases for "feature/*"
+    }
   ],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        releaseRules: [
+          { type: "chore", release: "patch" },
+          { type: "fix", release: "patch" },
+          { type: "feat", release: "minor" }
+        ]
+      }
+    ],
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     [
       "@semantic-release/github",
       {
         assets: [
-          { path: "dist/*.js", label: "JavaScript distribution" },
-          { path: "dist/*.map", label: "Source map" }
+          { path: ".next/**", label: "Next.js build files" }
         ]
       }
     ],
