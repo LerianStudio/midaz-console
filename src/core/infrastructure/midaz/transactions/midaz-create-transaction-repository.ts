b229e01a7e7d4ab387fdb3fac9944970f9e74360
paulo <paulo.barbosa@lerian.studio>
2025-02-05 10:29:15 -0300

    feat: httpfetchmidaz on transaction create route

--- src/core/infrastructure/midaz/transactions/midaz-create-transaction-repository.ts
+++ src/core/infrastructure/midaz/transactions/midaz-create-transaction-repository.ts
@@ -1,12 +1,19 @@
 import { TransactionEntity } from '@/core/domain/entities/transaction-entity'
 import { CreateTransactionRepository } from '@/core/domain/repositories/transactions/create-transaction-repository'
-import { HTTP_METHODS, httpMidazAuthFetch } from '../../utils/http-fetch-utils'
-import { injectable } from 'inversify'
+import { HTTP_METHODS } from '../../utils/http-fetch-utils'
+import { inject, injectable } from 'inversify'
+import { ContainerTypeMidazHttpFetch } from '../../container-registry/midaz-http-fetch-module'
+import { MidazHttpFetchUtils } from '../../utils/http-fetch-utils'
 
 @injectable()
 export class MidazCreateTransactionRepository
   implements CreateTransactionRepository
 {
+  constructor(
+    @inject(ContainerTypeMidazHttpFetch.MidazHttpFetchUtils)
+    private readonly midazHttpFetchUtils: MidazHttpFetchUtils
+  ) {}
+
   private baseUrl: string = process.env.MIDAZ_TRANSACTION_BASE_PATH as string
   async create(
     organizationId: string,
@@ -15,7 +22,9 @@ export class MidazCreateTransactionRepository
   ) {
     const url = `${this.baseUrl}/organizations/${organizationId}/ledgers/${ledgerId}/transactions/json`
 
-    const response = await httpMidazAuthFetch<TransactionEntity>({
+    const response = await this.midazHttpFetchUtils.httpMidazAuthFetch<
+      TransactionEntity
+    >({
       url,
       method: HTTP_METHODS.POST,
       body: JSON.stringify(transaction)
