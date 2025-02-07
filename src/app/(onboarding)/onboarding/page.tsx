bea00df7ba5127b069606e20250a92e46d9026e1
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2025-02-07 16:29:29 -0300

    :boom: fix: Fixed dialog style

--- src/app/(onboarding)/onboarding/page.tsx
+++ src/app/(onboarding)/onboarding/page.tsx
@@ -14,61 +14,47 @@ import Rocket from '@/animations/rocket.gif'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import React from 'react'
-import { FormattedMessage, useIntl } from 'react-intl'
+import { useIntl } from 'react-intl'
 
 const Page = () => {
   const intl = useIntl()
   const router = useRouter()
   const { data } = useListOrganizations({})
-  const [dialogOpen, setDialogOpen] = React.useState(true)
 
   return (
-    <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+    <Dialog open>
       <DialogContent
         showCloseButton={false}
-        className="[data-radix-dialog-close] w-full max-w-[544px] p-12 sm:min-w-[544px]"
-        onOpenAutoFocus={(event) => event.preventDefault()}
-        onInteractOutside={(event) => event.preventDefault()}
-        onEscapeKeyDown={(event) => event.preventDefault()}
+        className="[data-radix-dialog-close] w-full max-w-[640px] p-12 sm:min-w-[640px]"
       >
         <DialogHeader>
-          <p className="text-base font-medium text-zinc-600">
-            {intl.formatMessage({
-              id: 'homePage.dialog.onboarding.gettingStarted',
-              defaultMessage: 'Getting started'
-            })}
-          </p>
-
-          <div className="flex flex-row justify-between gap-12 pr-12">
-            <DialogTitle className="text-4xl font-bold text-zinc-600">
-              {intl.formatMessage({
-                id: 'homePage.dialog.onboarding.title',
-                defaultMessage: 'Launch the Midaz Console'
-              })}
-            </DialogTitle>
-
-            <div className="shrink-0">
-              <Image src={Rocket} alt="Rocket" height={64} width={64} />
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-col gap-8">
+              <p className="text-base font-medium text-zinc-600">
+                {intl.formatMessage({
+                  id: 'onboarding.dialog.firstSteps',
+                  defaultMessage: 'First steps'
+                })}
+              </p>
+              <DialogTitle className="text-4xl font-bold text-zinc-600">
+                {intl.formatMessage({
+                  id: 'onboarding.dialog.title',
+                  defaultMessage: 'Launch the Midaz Console'
+                })}
+              </DialogTitle>
+            </div>
+            <div className="flex items-center px-12">
+              <div className="shrink-0">
+                <Image src={Rocket} alt="Rocket" height={64} width={64} />
+              </div>
             </div>
           </div>
 
-          <DialogDescription className="space-y-7">
-            <p className="text-sm font-medium text-zinc-400">
-              <FormattedMessage
-                id="homePage.dialog.onboarding.description"
-                defaultMessage="Complete your Organization's registration and create the first Ledger to activate the powerful features of the Midaz Console."
-                values={{
-                  organization: (
-                    <span className="font-bold">
-                      <FormattedMessage
-                        id="organization"
-                        defaultMessage="Organization"
-                      />
-                    </span>
-                  )
-                }}
-              />
-            </p>
+          <DialogDescription className="space-y-7 text-sm font-medium text-zinc-400">
+            {intl.formatMessage({
+              id: 'onboarding.dialog.description',
+              defaultMessage: `Complete your Organization's registration and create the first Ledger to activate the powerful features of the Midaz Console.`
+            })}
           </DialogDescription>
         </DialogHeader>
 
@@ -79,7 +65,7 @@ const Page = () => {
             onClick={() => router.push('/settings')}
           >
             {intl.formatMessage({
-              id: 'homePage.dialog.startOnboarding.button',
+              id: 'onboarding.dialog.button',
               defaultMessage: "Let's go"
             })}
           </Button>
