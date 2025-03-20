25ae6d2ceef1751ee361f0c583751dfd0f9a6c49
Augusto Alvarenga <augusto.santesso@gmail.com>
2025-03-20 19:23:21 -0300

    fix: getStorageObject empty object excpetion

--- src/lib/storage/get-storage-object.ts
+++ src/lib/storage/get-storage-object.ts
@@ -3,7 +3,9 @@ import { getStorage } from './get-storage'
 export function getStorageObject(key: string, defaultValue: any) {
   try {
     const dataString = getStorage(key, defaultValue)
-    return JSON.parse(dataString)
+    const isEmpty = Object.keys(dataString).length === 0
+
+    return isEmpty ? {} : JSON.parse(dataString)
   } catch (error) {
     console.error(error)
     return defaultValue
