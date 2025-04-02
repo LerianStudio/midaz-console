b5145dfb5b9ae866cba3754f6aa88a7aea44d714
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2025-04-02 13:31:42 -0300

    feat: Refactor organization repositories

--- src/core/infrastructure/container-registry/midaz/midaz-module.ts
+++ src/core/infrastructure/container-registry/midaz/midaz-module.ts
@@ -2,16 +2,19 @@ import { AccountRepository } from '@/core/domain/repositories/account-repository
 import { Container, ContainerModule } from '../../utils/di/container'
 import { MidazAssetModule } from './asset-module'
 import { MidazLedgerModule } from './ledger-module'
-import { MidazOrganizationModule } from './organization-module'
 import { MidazPortfolioModule } from './portfolio-module'
 import { MidazSegmentModule } from './segment-module'
 import { MidazTransactionModule } from './transaction-module'
 import { BalanceRepository } from '@/core/domain/repositories/balance-repository'
 import { MidazBalanceRepository } from '@/core/infrastructure/midaz/repositories/midaz-balance-repository'
 import { MidazAccountRepository } from '@/core/infrastructure/midaz/repositories/midaz-account-repository'
+import { OrganizationRepository } from '@/core/domain/repositories/organization-repository'
+import { MidazOrganizationRepository } from '@/core/infrastructure/midaz/repositories/midaz-organization-repository'
 
 export const MidazModule = new ContainerModule((container: Container) => {
-  container.load(MidazOrganizationModule)
+  container
+    .bind<OrganizationRepository>(OrganizationRepository)
+    .to(MidazOrganizationRepository)
   container.load(MidazLedgerModule)
   container.load(MidazPortfolioModule)
   container
