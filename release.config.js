70e158c71c3ab3ed0fce78372e9b5c247d919ea2
LF Barrile <lf@lerian.studio>
2025-04-09 10:52:10 -0300

    feat: set configurations of release using JS file

--- release.config.js
+++ release.config.js
@@ -2,9 +2,9 @@ module.exports = {
   branches: [
     { name: 'main' }, // Versão final X.Y.Z
     { name: 'develop', prerelease: 'beta', channel: 'beta' }, // beta.N
-    { name: new RegExp('^feature/.+'), prerelease: 'alpha', channel: 'alpha' }, // alpha.N
-    { name: new RegExp('^release/.+'), prerelease: 'rc', channel: 'rc' }, // rc.N
-    { name: new RegExp('^hotfix/.+'), prerelease: 'hf', channel: 'hf' } // hf.N
+    { name: 'regex:^feature/.+', prerelease: 'alpha', channel: 'alpha' }, // alpha.N
+    { name: 'regex:^release/.+', prerelease: 'rc', channel: 'rc' }, // rc.N
+    { name: 'regex:^hotfix/.+', prerelease: 'hf', channel: 'hf' } // hf.N
   ],
   plugins: [
     [
@@ -24,7 +24,8 @@ module.exports = {
           { type: 'fix', release: 'minor' },
           { type: 'refactor', release: 'minor' },
           { type: 'docs', release: 'patch' },
-          { breaking: true, release: 'major' }
+          { breaking: true, release: 'major' },
+          { type: 'breaking', release: 'major' }
         ]
       }
     ],
@@ -69,4 +70,4 @@ module.exports = {
       }
     ]
   ]
-}
+};
