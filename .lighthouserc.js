5a2f1a441770541742bebd57235bad81fc8558db
LF Barrile <lf@lerian.studio>
2025-04-09 16:16:29 -0300

    feat: generate report on both types

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,45 +1,49 @@
 module.exports = {
-    ci: {
-      collect: {
-        startServerCommand: 'npm run start', // command to start the server
-        url: ['http://localhost:8081'],      // url to execute midaz-console app
-        numberOfRuns: 3,
-      },
-      assert: {
-        assertions: {
-          // ✅ Mantain on level 'warn' don't broke pipeline
-          'bf-cache': ['warn', { minScore: 0 }],
-          'color-contrast': ['warn', { minScore: 0 }],
-          'efficient-animated-content': ['warn', { maxLength: 1 }],
-          'errors-in-console': ['warn', { minScore: 0 }],
-          'html-has-lang': ['warn', { minScore: 0 }],
-          'meta-description': ['warn', { minScore: 0 }],
-          'robots-txt': ['warn', { minScore: 0 }],
-          'unused-javascript': ['warn', { maxLength: 1 }],
-          'uses-rel-preconnect': ['warn', { maxLength: 1 }],
-          'uses-responsive-images': ['warn', { maxLength: 1 }],
-          'interactive': ['warn', { minScore: 0 }],
-          'largest-contentful-paint': ['warn', { minScore: 0 }],
-          'legacy-javascript': ['warn', { maxLength: 1 }],
-          'render-blocking-resources': ['warn', { maxLength: 1 }],
+  ci: {
+    collect: {
+      startServerCommand: 'npm run start', // comando para iniciar o servidor
+      url: ['http://localhost:8081'],      // URL para executar o aplicativo midaz-console
+      numberOfRuns: 3,
+    },
+    assert: {
+      assertions: {
+        // ✅ Manter no nível 'warn' para não quebrar o pipeline
+        'bf-cache': ['warn', { minScore: 0 }],
+        'color-contrast': ['warn', { minScore: 0 }],
+        'efficient-animated-content': ['warn', { maxLength: 1 }],
+        'errors-in-console': ['warn', { minScore: 0 }],
+        'html-has-lang': ['warn', { minScore: 0 }],
+        'meta-description': ['warn', { minScore: 0 }],
+        'robots-txt': ['warn', { minScore: 0 }],
+        'unused-javascript': ['warn', { maxLength: 1 }],
+        'uses-rel-preconnect': ['warn', { maxLength: 1 }],
+        'uses-responsive-images': ['warn', { maxLength: 1 }],
+        'interactive': ['warn', { minScore: 0 }],
+        'largest-contentful-paint': ['warn', { minScore: 0 }],
+        'legacy-javascript': ['warn', { maxLength: 1 }],
+        'render-blocking-resources': ['warn', { maxLength: 1 }],
   
-          // 💡 Enable these lines below if you want to validate with a minimum score of 0.9 or another grade:
-          /*
-          'bf-cache': ['error', { minScore: 0.9 }],
-          'color-contrast': ['error', { minScore: 0.9 }],
-          'errors-in-console': ['error', { minScore: 0.9 }],
-          'html-has-lang': ['error', { minScore: 0.9 }],
-          'meta-description': ['error', { minScore: 0.9 }],
-          'robots-txt': ['error', { minScore: 0.9 }],
-          'interactive': ['error', { minScore: 0.9 }],
-          'largest-contentful-paint': ['error', { minScore: 0.9 }],
-          */
-        },
-      },
-      upload: {
-        target: 'filesystem',
-        outputDir: '.lighthouseci',
+        // 💡 Habilitar estas linhas abaixo se você quiser validar com uma pontuação mínima de 0.9 ou outra nota:
+        /*
+        'bf-cache': ['error', { minScore: 0.9 }],
+        'color-contrast': ['error', { minScore: 0.9 }],
+        'errors-in-console': ['error', { minScore: 0.9 }],
+        'html-has-lang': ['error', { minScore: 0.9 }],
+        'meta-description': ['error', { minScore: 0.9 }],
+        'robots-txt': ['error', { minScore: 0.9 }],
+        'interactive': ['error', { minScore: 0.9 }],
+        'largest-contentful-paint': ['error', { minScore: 0.9 }],
+        */
       },
     },
-  };
-  
+    upload: [
+      {
+        target: 'temporary-public-storage', // Enviar para armazenamento público temporário
+      },
+      {
+        target: 'filesystem',              // Salvar localmente
+        outputDir: '.lighthouseci',        // Diretório onde os relatórios serão salvos
+      }
+    ]
+  }
+};
