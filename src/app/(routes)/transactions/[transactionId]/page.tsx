7179f3ccc622377533d11c56369e762042ea2e92
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2025-04-15 18:01:52 -0300

    feat: Removed usePopulateForm usage

--- src/app/(routes)/transactions/[transactionId]/page.tsx
+++ src/app/(routes)/transactions/[transactionId]/page.tsx
@@ -40,7 +40,7 @@ import { truncateString } from '@/helpers'
 import dayjs from 'dayjs'
 import { OperationDto } from '@/core/application/dto/transaction-dto'
 import { TRANSACTION_DETAILS_TAB_VALUES } from './transaction-details-tab-values'
-import { usePopulateForm } from '@/lib/form'
+import { getInitialValues } from '@/lib/form'
 
 const DEFAULT_TAB_VALUE = TRANSACTION_DETAILS_TAB_VALUES.SUMMARY
 
@@ -69,6 +69,7 @@ export default function TransactionDetailsPage() {
 
   const form = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
+    values: getInitialValues(initialValues, transaction),
     defaultValues: initialValues
   })
 
@@ -85,8 +86,6 @@ export default function TransactionDetailsPage() {
 
     return parsed.toString()
   }
-
-  usePopulateForm(form, transaction)
 
   if (isLoading) {
     return <SkeletonTransactionDialog />
