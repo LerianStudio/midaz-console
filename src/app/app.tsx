89af2afa0d704c9d76bfbb739bb72bdfee5a06c0
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2025-04-15 15:09:04 -0300

    feat: Removed old toast

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,7 +3,6 @@ import '@/app/globals.css'
 import { QueryProvider } from '@/providers/query-provider'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from '@/components/ui/toast/toaster'
-import { Toaster as RHTToaster } from 'react-hot-toast'
 import { LocalizationProvider } from '@/lib/intl'
 import { ThemeProvider } from '@/lib/theme'
 import ZodSchemaProvider from '@/lib/zod/zod-schema-provider'
@@ -17,10 +16,6 @@ export default async function App({ children }: { children: React.ReactNode }) {
           <ThemeProvider>
             <ZodSchemaProvider>
               {children}
-              <RHTToaster
-                position="top-right"
-                containerStyle={{ top: 60, right: 60 }}
-              />
               <Toaster />
             </ZodSchemaProvider>
           </ThemeProvider>
